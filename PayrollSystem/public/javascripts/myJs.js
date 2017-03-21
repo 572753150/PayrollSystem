d8849432f1faaede394e33d5c13655186913e208
xiangyang guo <guo.xiangya@uwlax.edu>
2017-03-20 23:24:23 -0500

    html

--- PayrollSystem/public/javascripts/myJs.js
+++ PayrollSystem/public/javascripts/myJs.js
@@ -1,6 +1,8 @@
 var metaData;
-var level;
+var levels;
 var guessesNum;
+var defaultsOfMeta;
+var csrf;
 function colorGroup(guessBackground, textBackground, wordBackground) {
     this.guessBackground = guessBackground;
     this.textBackground = textBackground;
@@ -16,8 +18,11 @@ var state = {
 function setPage(page) {
     state.page.page = page;
     if (page == "login") {
+        $('body').addClass('background');
     } else {
+        $('body').removeClass('background');
     }
+
     if (page == 'content') {
 
     }
@@ -30,40 +35,42 @@ function setPage(page) {
     );
 }
 
-function setUser(user) {
-    state.user = user;
-    $('#email').text(user && user.email);
-    setPage(user ? 'content' : 'login')//这里有什么情况
-}
 
 function login(evt) {
     evt.preventDefault();
     var password = $('#password').val();
     var username = $('#username').val();
+    var reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/;
 
     $('#password').val('');
     $('#username').val('');
-    if (username != '' && password != '') {
+    if (username != '' && password != ''&& reg.test(username)) {
         $.ajax({
             url: "/wordgame/api/v2/login",
             data: {"username": username, "password": password},
             method: 'POST',
-            success: function (data) {
+            success: function (data, status,request) {
                 if (data.msg) {
                     alert(data.msg)
                 } else {
-                    loadResourece();
+                    csrf=request.getResponseHeader("csrf");
                     setUser(data);
-                    createTable(data.email);
+                    $(".useless").removeAttr("onchange");
+                    loadResourece();
+                    createTable(data.email)
+                    $(".useless").attr("onchange","updateDefaults();");
+
                 }
             }
         })
     } else {
-        alert("Please enter username or password!")
+        alert("Please enter valid username and password!")
     }
 }
 
 function loadResourece() {
+
+    $(".useless").removeAttr("onchange");
     $.ajax({
         url: '/wordgame/api/v2/meta/fonts',
         method: 'GET',
@@ -73,91 +80,137 @@ function loadResourece() {
         },
     });
 
+        $.ajax({
+            url: '/wordgame/api/v2/meta',
+            method: 'GET',
+            success: function (data) {
+                metaData = data;
+                levels = metaData.levels;
+                defaultsOfMeta=metaData.defaults;
+                showLevels();
+                setUserDefaults();
+                $(".useless").attr("onchange","updateDefaults();");
 
-    $.ajax({
-        url: '/wordgame/api/v2/meta',
-        method: 'GET',
-        success: function (data) {
-            metaData = data;
-            level = metaData.levels;
-            var initalColor = metaData.defaults.colors;
-            $('input#guessColor').val(initalColor.guessBackground);
-            $('input#textBackground').val(initalColor.textBackground);
-            $('input#wordColor').val(initalColor.wordBackground)
-        },
-        error: function (xhr, status, errorThrown) {
-            alert("Sorry, there was a problem!" + errorThrown);
-        },
-    });
+            },
+            error: function (xhr, status, errorThrown) {
+                alert("Sorry, there was a problem!" + errorThrown);
+            },
+        });
+
+}
+
+
+function showLevels() {
+
+    var levelNOde = $('#level');
+    for(i in levels) {
+        levelNOde.append("<option id='" + levels[i].name + "'>" + levels[i].name + "</option>")
+    }
+}
+
+function setUserDefaults() {
+    if (state.user&&state.user.defaults&&state.user.defaults.font){
+        $("#font").val(state.user.defaults.font.family);
+        $("#level").val(state.user.defaults.level.name);
+        $('input#guessColor').val(state.user.defaults.colors.guessBackground);
+        $('input#textColor').val(state.user.defaults.colors.textBackground);
+        $('input#wordColor').val(state.user.defaults.colors.wordBackground)
+    }else {
+        $("#font").val(defaultsOfMeta.font.family);
+        $("#level").val(defaultsOfMeta.level.name);
+        $('input#guessColor').val(defaultsOfMeta.colors.guessBackground);
+        $('input#textColor').val(defaultsOfMeta.colors.textBackground);
+        $('input#wordColor').val(defaultsOfMeta.colors.wordBackground)
+    }
 }
 
 function logout(evt) {
-
     $.ajax({
         url: '/wordgame/api/v2/logout',
         method: 'POST',
         success: function () {
             setUser(null);
+            csrf='';
+            $(".useless").removeAttr("onchange");
+
+            $("#level").empty();
+            $("#font").empty();
+
         }
-
     });
-
 }
 
-$(document).ready(function () {
+// $(document).ready(function () {
+//     $.ajax({
+//         url: '/wordgame/api/v2/user',//check 是否有user
+//         method: 'GET',
+//         success: function (user,status,request) {
+//             csrf=request.getResponseHeader('csrf');
+//             $('body').show().addClass('background');
+//             setUser(user);
+//             createTable(user.email);
+//             $(".useless").removeAttr("onchange");
+//             loadResourece();
+//         },
+//         error: () => {
+//             $('body').show().addClass('background');
+//             setPage('login');
+//         }
+//     });
+// })
 
-    $.ajax({
-        url: '/wordgame/api/v2/user',//check 是否有user
-        method: 'GET',
-        success: (user)=> {
-            // $('body').show().addClass('background');
-            setUser(user);
-            createTable(user.email);
-        },
-        error: () => {
-            // $('body').show().addClass('background');
-            setPage('login');
-        }
-    });
-
-
-    $.ajax({
-        url: '/wordgame/api/v2/meta/fonts',
-        method: 'GET',
-        success: showFonts,
-        error: function (xhr, status, errorThrown) {
-            alert("Sorry, there was a problem!" + errorThrown);
-        },
-    });
-
-
-    $.ajax({
-        url: '/wordgame/api/v2/meta',
-        method: 'GET',
-        success: function (data) {
-            metaData = data;
-            level = metaData.levels;
-            var initalColor = metaData.defaults.colors;
-            $('input#guessColor').val(initalColor.guessBackground);
-            $('input#textBackground').val(initalColor.textBackground);
-            $('input#wordColor').val(initalColor.wordBackground)
-        },
-        error: function (xhr, status, errorThrown) {
-            alert("Sorry, there was a problem!" + errorThrown);
-        },
-    });
-
-})
+function setUser(user) {
+    state.user = user;
+    $('#email').text(user && user.email);//
+    setPage(user ? 'content' : 'login')//这里有什么情况
+}
 
 function showFonts(data) {
-    var fontNOde = $('#font');
-    var head = $('#head');
-    var fontList = data;
-    for (i in fontList) {
-        var fontObject = fontList[i];
-        var name = fontObject.family;
-        fontNOde.append("<option>" + name + "</option>");
+        var fontNOde = $('#font');
+        var head = $('#head');
+        var fontList = data;
+        for (i in fontList) {
+            var fontObject = fontList[i];
+            var name = fontObject.family;
+            fontNOde.append("<option>" + name + "</option>");
+        }
+}
+
+function validateCsrf(data) {
+    if(data.msg&&data.msg=="logout"){
+        logout();
+        return true;
+    }else {
+        return false;
     }
+}
+
+
+function updateDefaults() {
+    var colors = new colorGroup($('input#guessColor').val(), $('input#textColor').val(), $('input#wordColor').val());
+    var fontsList=metaData.fonts;
+    var fontObject;
+    for (var i = 0 in fontsList) {
+        if (fontsList[i].family == $("#font").val()) {
+            fontObject = fontsList[i];
+            break;
+        }
+    }
+    var level=metaData.levels[$("#level").val()]
+    var defaults=new Default(colors,level,fontObject);
+
+    $.ajax({
+        method:'PUT',
+        url:"/wordgame/api/v2/"+state.user.email+"/defaults",
+        data: defaults,
+        headers: {'csrf':csrf},
+        success:function (data) {
+            if(validateCsrf(data)){return};
+            if(data.msg){
+                alert(data.msg);
+            }
+        }
+    })
 }
 
 function createGame() {
@@ -165,11 +218,13 @@ function createGame() {
     $("#createGame").hide();
     var colors = new colorGroup($('input#guessColor').val(), $('input#textColor').val(), $('input#wordColor').val());
     $.ajax({
-        type: 'POST',
+        method: 'POST',
         url: '/wordgame/api/v2/' + state.user.email + "?level=" + $('#level').val(),
         data: colors,
-        headers: {'X-font': $('#font').val()},
+        headers: {'X-font': $('#font').val(),'csrf':csrf},
         success: function (data) {
+            if(validateCsrf(data)){return};
+
             if (data.msg == undefined) {
                 state.game = data;
                 $("#head").append("<link rel='stylesheet' href='" + state.game.font.url + "'>");
@@ -227,17 +282,6 @@ function klose() {
     createTable();
 }
 
-// function updateTabe() {
-//     $($("[name=" + state.game.id + "]").parent().children("th")[1]).empty();
-//     for (var i in state.game.view) {
-//         var label = "<label style='background-color: " + state.game.colors.wordBackground + ";font-size: 30px ;color:" + state.game.colors.textBackground + ";font-family:" + state.game.font.family + "' class='labelOfWord gap' >" + state.game.view[i].toUpperCase() + "</label>";
-//         $($("[name=" + state.game.id + "]").parent().children("th")[1]).append(label);
-//     }
-//     $($("[name=" + state.game.id + "]").parent().children("th")[2]).text(state.game.remaining);
-//     $($("[name=" + state.game.id + "]").parent().children("th")[3]).text(state.game.target);
-//     $($("[name=" + state.game.id + "]").parent().children("th")[4]).text(state.game.status);
-// }
-
 function guess() {
     if ((!(/^[A-Za-z]+$|'|-/.test($("[name='inputOfGuess']").val()))) || $("[name='inputOfGuess']").val().length > 1) {
         $("[name='inputOfGuess']").val("");
@@ -249,9 +293,11 @@ function guess() {
         return;
     }
     $.ajax({
-        type: 'POST',
+        method: 'POST',
         url: "/wordgame/api/v2/" + state.user.email + "/" + state.game.id + "/" + "guesses" + "?guess=" + $("[name='inputOfGuess']").val(),
+        headers: {'csrf':csrf},
         success: function (data) {
+            if(validateCsrf(data)){return};
             state.game = data;
             guessesNum = state.game.remaining;
             $("label#remaining" + state.game.id + "").text(function () {
@@ -259,14 +305,16 @@ function guess() {
             });
             changeView();
             judgeStatus(state.game);
+            var label = "<label style='background-color: " + state.game.colors.guessBackground + ";font-size: 40px ;color:" + state.game.colors.textBackground + ";font-family:" + state.game.font.family + "' class='labelOfWord gap' value='" + $("[name='inputOfGuess']").text() + "' >" + $("[name='inputOfGuess']").val().toUpperCase() + "</label>";
+            $('[name="div2"]').append(label)
+            $("[name='inputOfGuess']").val('');//
+
         },
         error: function (xhr, status, errorThrown) {
             alert("Sorry, there was a problem!" + errorThrown);
         },
     });
-    var label = "<label style='background-color: " + state.game.colors.guessBackground + ";font-size: 40px ;color:" + state.game.colors.textBackground + ";font-family:" + state.game.font.family + "' class='labelOfWord gap' value='" + $("[name='inputOfGuess']").text() + "' >" + $("[name='inputOfGuess']").val().toUpperCase() + "</label>";
-    $('[name="div2"]').append(label)
-    $("[name='inputOfGuess']").val('');
+
 }
 
 
@@ -276,8 +324,9 @@ function createTable() {
     $.ajax({
         url:'/wordgame/api/v2/'+state.user.email+'/games',
         method:"GET",
+        headers: {'csrf':csrf},
         success:function (data) {
-            console.log(data)
+            if(validateCsrf(data)){return};
             data.forEach( game=> {
                 $("#head").append("<link rel='stylesheet' href='" + game.font.url + "'>");
                 $('#table').append("<tr><th>" + game.level.name + "</th><th name='" + game.id + "'></th><th>" + game.remaining + "</th><th style='text-transform:uppercase'>" + game.target + "</th><th>" + game.status + "</th> </tr>>");
@@ -288,7 +337,6 @@ function createTable() {
                 $("[name='" + game.id + "']").click(function (event) {
                     var currentElement = event.currentTarget;
                     var gameId = $(currentElement).attr("name");
-                    console.log(gameId)
                     $("#second").hide();
                     resumeGame(gameId);
                 })
@@ -305,7 +353,9 @@ function resumeGame(gameid) {
     $.ajax({
         url: '/wordgame/api/v2/' + state.user.email + '/' + gameid,
         method: 'GET',
+        headers: {'csrf':csrf},
         success: function (data) {
+            if(validateCsrf(data)){return};
             state.game = data;
             createViewPage();
             judgeStatus(state.game);
@@ -334,4 +384,10 @@ function judgeStatus(game) {
         $(".guessForFirsline").hide();
         $("[name='divWell']").attr("style", "background:url(http://charity.cs.uwlax.edu/views/cs402/homeworks/hw2/images/winner.gif)")
     }
+}
+
+function Default(colors, level, font) {
+    this.colors=colors;
+    this.level=level;
+    this.font=font;
 }
---
+++ PayrollSystem/public/javascripts/styleControl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by apple on 2017/3/20.
+ */
+$(document).ready(function () {
+    $("#form_new_employee").submit(function(e) {
+        e.preventDefault();
+    });
+    }
+)
+
+
